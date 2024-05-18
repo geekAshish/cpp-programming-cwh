e4508008f84117063e30681a9262c9d1e234f440
geekAshish <ashishkushwaha472004@gmail.com>
2024-05-18 23:23:06 +0530

    changes

---
+++ 1_basic.cpp
@@ -0,0 +1,6 @@
+#include<iostream> // header file
+
+int main() {
+  std::cout<<"This is my first c++ programme!";
+  return 0;
+}
---
+++ 2_variable_and_comments.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// Variables are like container, to store a specific data
+
+
+int main() {
+  int sum = 9;
+
+  std::cout<<"This code is good";
+  std::cout<<"This code is good"<<sum;
+  
+  return 0;
+}
