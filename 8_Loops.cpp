cc2732d7d9b3792c4fea33ecd23a2422cd18e1a7
geekAshish <ashishkushwaha472004@gmail.com>
2024-05-21 23:01:33 +0530

    for loop, while loop, do-while loop

---
+++ 8_Loops.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+  // For loop
+  // for (int i = 0; i < 6; i++)
+  // {
+  //   cout<<"this is : "<<i+1<<endl;
+  // }
+
+
+  // while loop
+  // int i = 0;
+  // while (i < 4)
+  // {
+  //   cout<<"this is "<<i<<endl;
+  //   i++;
+  // }
+
+
+  // do-while loop
+  int i = 1;
+  do
+  {
+    cout<<"this is : "<< i << endl;
+    i++;
+  }
+  while (i <= 5);
+  
+
+
+  return 0;
+}
