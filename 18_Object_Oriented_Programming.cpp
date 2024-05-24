23e87251711dff3d6b02d5d2d97c09c48277269c
geekAshish <ashishkushwaha472004@gmail.com>
2024-05-24 21:29:54 +0530

    POP, why OPPs,  OOPs, basic concepts, benefits of OPPs

---
+++ 18_Object_Oriented_Programming.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+// # WHY OOPS?
+// 1. C++ language was designed with the main intention of adding object-oriented features to C language.
+// 2. As the size of the program increases, readability, maintainability and bug-free nature of programs decreases.
+// 3. This was the major problem with languages like C which relied upon functions or procedures (hence the name procedural programming language)
+// 4. As a result, the possibility of not addressing the problem in an effective manner was high.
+// 5. Also, as data was almost neglected, data security was easily compromised.
+// 6. Using classes solves this problem by modelling program as a real world scenario
+
+// # PROCEDURE ORIENTED PROGRAMMING (POP)
+// 1. Consists of writing a set of instructions for the computer to follow
+// 2. Main focus is on functions and not on flow of data
+// 3. Functions can either use local or global data
+// 4. Data moves openly from function to function
+
+// # OBJECT ORIENTED PROGRAMMING
+// 1. Works on the concept of classes and objects
+// 2. A class is a template to create objects
+// 3. Treats data as a critical element
+// 4. Decomposes the problem in objects and builds data and functions around the objects
+
+// BASIC CONCEPTS IN OBJECT ORIENTED PROGRAMMING
+// Classes — Basic template for creating objects.
+// Objects — Basic run time entities.
+// Data Abstraction & Encapsulation — Wrapping data and functions into single unit.
+// Inheritance — Properties of one class can be inherited into others.
+// Polymorphism — ability to take more than one forms.
+// Dynamic Binding — code which will execute is not known until the program runs.
+// Message Passing — Object.message(Information) call format.
+
+// # BENEFITS OF OBJECT ORIENTED PROGRAMMING
+// 1. Better code reusability using objects and Inheritance.
+// 2. Principle of data hiding helps build secure systems.
+// 3. Multiple objects can co-exist without any interference.
+// 4. Software complexity can be easily managed.
+
+int main()
+{
+
+  return 0;
+}
