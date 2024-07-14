20c00b16d013c21f286fa4e284bb4c011d9fb9ad
geekAshish <ashishkushwaha472004@gmail.com>
2024-07-14 15:13:31 +0530

    Polymorphism and It's Type

---
+++ 46_Polymorphism.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+  // Polymorphism
+  // - one name multiple form
+  // - Type of Polymorphism
+  // 1. Compile Time Polymorphism
+  //    - 1.1 Function Overloading
+  //    - 1.2 operator overloading
+  // 2. Run Time Polymorphism
+  //    - 1.1 Virtual Function
+
+  return 0;
+}
