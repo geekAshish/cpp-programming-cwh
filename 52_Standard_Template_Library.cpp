5e586906fd7f9a98b88b489ee051a5eb00ea2665
geekAshish <ashishkushwaha472004@gmail.com>
2024-07-21 13:31:14 +0530

    Standard template library (STL)

---
+++ 52_Standard_Template_Library.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+/*
+Introduction to STL
+Library of Generic classes and function
+
+Why use STL?
+Well tested components
+Time saving
+
+Components of STL?
+1. Container        -->   Object which stores data
+i. Sequence container - store data in linear fashion - vector, list, dequeue
+ii. Associative container - Direct(fast) access - tree, set, multiset, map, multimap
+iii. Derived container - Real world modeling - stack, queue, priority-queue
+
+2. Algorithms       -->   Procedure to process data
+3. Iterators        -->   Object which points to an element of a container
+
+When to use which?
+Sequence Container
+1. vector - Random access - fast
+          - Insertion | deletion in middle - slow
+          - Insertion at the end - fast
+
+2. List - Random access - slow
+        - Insertion in middle - fast
+        - Insertion | deletion at the end - fast
+
+Associative container --> All operations fast except Random access
+
+Derived container --> Depends --> data structure
+
+*/
+
+int main() {
+
+
+  return 0;
+}
